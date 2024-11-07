2820a89e6adb7730510a5dbf997be755c55b13f4
ShivaniMadda <shivanimadda04@gmail.com>
2024-11-07 12:20:20 +0530

    first commit

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.BucketV2('demo-bucket1')
+bucket = s3.BucketV2('ademo1-bucket1')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
